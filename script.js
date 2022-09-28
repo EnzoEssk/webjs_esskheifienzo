9e837b2b646471f40bd75e0021df47322bf36d4c
EnzoEssk <enzoesre@gmail.com>
2022-09-28 15:07:23 +0200

    cours

---
+++ script.js
@@ -0,0 +1,6 @@
+const myAnchor = document.getElementsByTagName('my-anchor');
+console.log(myAnchor);
+
+const newElt = document.createElement("div");
+let elt = document.getElementById("block4");
+elt.apprendChild(newElt);
